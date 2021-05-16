e62cee0e7e4479cd72ca06d45bfdf0d6b4a23c7b
exepirit <exepir1t@protonmail.com>
2021-05-16 12:56:14 +0700

    feat: add Kubernetes as domain source

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.15
 
 require (
 	github.com/cloudflare/cloudflare-go v0.13.4
-	github.com/gin-gonic/gin v1.6.3
-	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/pkg/errors v0.9.1
-	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
+	k8s.io/api v0.21.0
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
 )
---
+++ source/kubernetes/source.go
@@ -0,0 +1,89 @@
+package kubernetes
+
+import (
+	"context"
+
+	"github.com/exepirit/cf-ddns/domain"
+	"github.com/pkg/errors"
+	core "k8s.io/api/core/v1"
+	networking "k8s.io/api/networking/v1beta1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type Source struct {
+	client *kubernetes.Clientset
+}
+
+func (s *Source) GetEndpoints() ([]*domain.Endpoint, error) {
+	ctx := context.Background()
+
+	domains, err := s.getDomainNames(ctx)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to get domains")
+	}
+
+	targets, err := s.getTargets(ctx)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to get targets")
+	}
+
+	endpoints := make([]*domain.Endpoint, len(domains))
+	for i, d := range domains {
+		endpoints[i] = domain.NewEndpoint(d, domain.RecordTypeA, targets...)
+	}
+	return endpoints, nil
+}
+
+func (s *Source) getTargets(ctx context.Context) (domain.Target, error) {
+	nodesClient := s.client.CoreV1().Nodes()
+	nodes, err := nodesClient.List(ctx, meta.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	target := make(domain.Target, 0)
+	for _, node := range nodes.Items {
+		addr := s.getNodeAddress(node)
+		if addr != "" {
+			target = append(target, addr)
+		}
+	}
+	return target, nil
+}
+
+func (*Source) getNodeAddress(node core.Node) string {
+	addresses := node.Status.Addresses
+
+	for _, addr := range addresses {
+		if addr.Type == core.NodeExternalIP {
+			return addr.Address
+		}
+	}
+
+	return ""
+}
+
+func (s *Source) getDomainNames(ctx context.Context) ([]string, error) {
+	ingresses, err := s.getIngresses(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	domains := make([]string, 0)
+	for _, ingress := range ingresses {
+		for _, rule := range ingress.Spec.Rules {
+			domains = append(domains, rule.Host)
+		}
+	}
+	return domains, nil
+}
+
+func (s *Source) getIngresses(ctx context.Context) ([]networking.Ingress, error) {
+	ingressesClient := s.client.NetworkingV1beta1().Ingresses("default")
+	ingresses, err := ingressesClient.List(ctx, meta.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return ingresses.Items, nil
+}
